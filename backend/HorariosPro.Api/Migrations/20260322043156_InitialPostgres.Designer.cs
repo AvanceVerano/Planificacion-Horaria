ead7ebe6e944b50001d78a5406e336741c1d6d7f
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2026-03-21 23:32:36 -0500

    fix: version 8.0

--- backend/HorariosPro.Api/Migrations/20260322043156_InitialPostgres.Designer.cs
+++ backend/HorariosPro.Api/Migrations/20260322043156_InitialPostgres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HorariosPro.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HorariosPro.Api.Migrations
 {
     [DbContext(typeof(HorariosProDbContext))]
-    partial class HorariosProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260322043156_InitialPostgres")]
+    partial class InitialPostgres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
--- backend/HorariosPro.Api/Program.cs
+++ backend/HorariosPro.Api/Program.cs
@@ -4,7 +4,6 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<HorariosPro.Api.Services.HorarioGenerator>();
 builder.Services.AddEndpointsApiExplorer();
@@ -66,7 +65,6 @@ app.UseCors("AllowVercel");
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
